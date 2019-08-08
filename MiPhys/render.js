d0713d029273ef46bf94d39b782097a5e9b3d50c
rikedyp <rikedyp@users.noreply.github.com>
2019-08-08 13:22:48 +0100

    Render slightly nicer

--- MiPhys/render.js
+++ MiPhys/render.js
@@ -43,19 +43,23 @@ function Rnext() {
 
 function RenderCanvas() {
     if (posQueue.length === 0) {return;}
-
+    
     var pos = posQueue.shift() 
     var c = document.getElementById("viewport");
+	c.width = 300;
+	c.height = 300;
     var ctx = c.getContext("2d");
-
+    ctx.imageSmoothingEnabled = true;
     var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
     grd.addColorStop(0, "red");
     grd.addColorStop(1, "white");
 
     ctx.clearRect(0, 0, c.width, c.height);
+	
     pos.forEach(function(pos){
         ctx.beginPath();
         ctx.arc(pos[0]*c.width,pos[1]*c.height,5,0,2*Math.PI);
+		//ctx.arc(pos[0],pos[1],10,0,2*Math.PI);
         ctx.closePath()
         
         ctx.fillStyle='white';
