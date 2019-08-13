aeae6f19ac7e29b8f6d3e8f1408b14e6e7a457b1
rikedyp <rikedyp@users.noreply.github.com>
2019-08-13 08:58:00 +0100

    Basic 3D with Phoria

--- MiPhys/3drender.js
+++ MiPhys/3drender.js
@@ -1,17 +1,28 @@
 var posQueue = [];
+var pos;
 var reQueue = 0;
 var Rrunning=0;
 var requesting=0;
 var Rtimer;
+//var atoms = [];
+//var scene;
 
 var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
                        window.mozRequestAnimationFrame || window.msRequestAnimationFrame || 
                        function(c) {window.setTimeout(c, 15)};
+
 /**
    Phoria
    pho·ri·a (fôr-)
    n. The relative directions of the eyes during binocular fixation on a given object
 */					   
+
+var camx = 0.0;
+var spin = 0.0;
+var scale = 6.0;
+
+// Get canvas DOM
+var canvas = document.getElementById("viewport");
 
 function StartStop(){
     if (posQueue==[]){
@@ -30,12 +41,10 @@ function StartStop(){
 
 function Rnext() {
     RenderCanvas();
-    //console.log(posQueue.length);
+    console.log(posQueue.length);
     if (reQueue < 5 && posQueue.length < 1000) {
-	   //console.log("LENGTH");
-	   //console.log(posQueue.length);
-        requesting=1;
-	   reQueue+=1;
+		requesting=1;
+	    reQueue+=1;
         var data = new FormData();
         data.append("_callback", "Tick");
         var xhr = new XMLHttpRequest();
@@ -54,24 +63,43 @@ function Rnext() {
     Ttimer = setTimeout("Rnext()", 17);
 }
 
+//function createAtom(){
+	//console.log("createAtom");
+	
+	
+    //for (i=0;i<pos[0].length;i++){
+		
+	//}	
+	
+	
+	//for (i=0;i<pos.length;i++){
+//	atoms.push(atom);
+//	scene.graph.push(atom);
+	//}
+	//		pos[i] = pos[i].map((a, j) => a * scale);
+			
+		//	atom.translate(pos[i]);				
+//}
+
+//var atomstyle = Phoria.Entity.createStyle()
+//style = 
 
 function RenderCanvas() {
+
     if (posQueue.length === 0) {return;}
-    // Get canvas DOM
-    var canvas = document.getElementById("viewport");
+    
     // Set up scene 
     var scene = new Phoria.Scene();
-	var camx = 0.0;
-	var spin = 0.0;
-    scene.camera.position = {x:camx, y:5.0, z:-15.0};	
+	//createAtoms();
+    scene.camera.position = {x:camx, y:5.0, z:-55.0};	
     scene.perspective.aspect = canvas.width / canvas.height;
     scene.viewport.width = canvas.width;
     scene.viewport.height = canvas.height;
     // create a canvas renderer
     var renderer = new Phoria.CanvasRenderer(canvas);
-    var pos = posQueue.shift() 
+    pos = posQueue.shift() 
     // add a grid to help visualise camera position etc.
-    var plane = Phoria.Util.generateTesselatedPlane(8,8,0,20);
+    var plane = Phoria.Util.generateTesselatedPlane(20,20,-10,20);
     scene.graph.push(Phoria.Entity.create({
       points: plane.points,
       edges: plane.edges,
@@ -83,54 +111,89 @@ function RenderCanvas() {
          objectsortmode: "back"
       }
     }));
-    scene.graph.push(new Phoria.DistantLight());	
-	pos.forEach(function(pos){
-		console.log(pos);
-		scene.graph.push(Phoria.Entity.create({
-		//points: [{x:0.0,y:0.0,z:0.0}],
-		points:[{x:pos[0],y:pos[1],z:pos[2]}],
-			style:{
-				color: [10,200,150],
-				drawmode: "point",
-				shademode: "lightsource",
-				linewidth: 10,
-				linescale: 1
-			}
-		}));
-	})
-    // scene.graph.push(Phoria.Entity.create({
-		// points: [{x:0.0,y:0.0,z:0.0}],		
+    scene.graph.push(new Phoria.DistantLight());
+
+    
+	//pos.forEach(pushAtom(pos, b, scene));
+	// Loop over particles
+	//console.log("Render");
+	//var s = Phoria.Util.generateSphere(0.1,5,5);
+	// for (atom of atoms){
+		// console.log(atom);
+		// atom.translate()
+	// }
+	var s = Phoria.Util.generateSphere(0.1,5,5);
+	// var atomstyle = Phoria.Entity.createStyle({
+		// points: s.points,
+		// edges: s.edges,
+		// polygons: s.polygons,
 		// style:{
-			// color:[0,100,100],
-			// drawmode: "point",
-			// shademode: "lightsource",
-			// linewidth: 5,
-			// linescale: 2
+			// drawmode: "solid",
+			// diffuse: 1,
+			// specular: 16,
+			// shademode: "lightsource"
 		// }
-          		
-	// }));
+	// });
+	for (i=0;i<pos.length;i++){
+		var atom = Phoria.Entity.create({
+			points: s.points,
+			edges: s.edges,
+			polygons: s.polygons,
+			style:{
+				drawmode: "solid",
+				diffuse: 1,
+				specular: 16,
+				shademode: "lightsource"
+			}});
+		//atom.style = atomstyle;
+		scene.graph.push(atom);
+		pos[i] = pos[i].map((a, j) => a * scale);
+		// console.log(atoms[i]);
+		//console.log(atoms.length);
+		// atoms[i].translate(pos[i]);
+		//
+		//atom.translate(pos[i]);
+		// 3D Spheres for pretty particles
+		
+		//var axes = Object.keys(s.points[0]);
+		// Loop over sphere defining points
+		//for (j=0;j<s.points.length;j++) {
+		// var atom = Phoria.Entity.create({
+			// points: s.points,
+			// edges: s.edges,
+			// polygons: s.polygons,
+			// style:{
+				// diffuse: 1,
+				// specular: 16,
+				// shademode: "lightsource"
+				// }
+		// });
+		//console.log(scene.graph[i]);
+		
+		
+		atom.translate(pos[i]);				
+		//}
+	}
+	//pushAtom(pos[0],b,scene);
+	
     var fnAnimate = function() {
 		camx=10*Math.sin(spin+=0.01);
 		scene.camera.position = {x:camx, y:5.0, z:-15.0};
 		scene.modelView();
 		renderer.render(scene);
-		requestAnimFrame(fnAnimate);
-}
-requestAnimFrame(fnAnimate);
-    //var ctx = c.getContext("2d");
-    //canvas.width = 300
-    //canvas.height = 300
-    // var grd = canvas.createRadialGradient(75, 50, 5, 90, 60, 100);
-    // grd.addColorStop(0, "red");
-    // grd.addColorStop(1, "white");
+		//requestAnimFrame(fnAnimate);
+	}
+	
+	
+	
+			// s.points.forEach(point){
+			// for (var axis of axes){
+				// points[axis]+=pos[axis];
+			// }
+		// }
+	    
+	
+	
+	requestAnimFrame(fnAnimate);
+};
 
-    // canvas.clearRect(0, 0, c.width, c.height);
-    // pos.forEach(function(pos){
-        // canvas.beginPath();
-        // canvas.arc(pos[0]*c.width,pos[1]*c.height,1,0,2*Math.PI);
-        // canvas.closePath()
-        
-        // canvas.fillStyle='white';
-        // ctx.fillStyle = grd;
-        // ctx.fill();})
-}
