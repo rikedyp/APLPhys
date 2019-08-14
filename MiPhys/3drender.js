1c6f2c752f775d2a7cd2087d548ccb88d5fac96b
rikedyp <rikedyp@users.noreply.github.com>
2019-08-14 18:25:31 +0100

    Babylon particles

--- MiPhys/3drender.js
+++ MiPhys/3drender.js
@@ -19,70 +19,144 @@ var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnima
 
 var camx = 0.0;
 var spin = 0.0;
-var scale = 6.0;
+var scale = 12.0;
 
 // Get canvas DOM
 var canvas = document.getElementById("viewport");
+var baby;
 
 function StartStop(){
-    if (posQueue==[]){
-        new jBox("Modal", {"content":"<span>Run the script before starting the simulation<\/span>","onInit":function() { this.open(); }});
-    }
     if (!Rrunning){
         Rnext();
-        //top.frames[0].Trun();
     }
     else {
         clearTimeout(Ttimer);
-        //top.frames[0].Tstop();
     }
     Rrunning = !Rrunning;
 }
 
-function Rnext() {
-    RenderCanvas();
-    console.log(posQueue.length);
-    if (reQueue < 5 && posQueue.length < 1000) {
-		requesting=1;
-	    reQueue+=1;
-        var data = new FormData();
-        data.append("_callback", "Tick");
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', "/BrazilNut.mipage", true);
-        xhr.setRequestHeader("isAPLJax", true);
-        xhr.onreadystatechange = function() {                                   
-            if (this.readyState == 4 && this.status == 200) {                                       
-                APLJaxReturn(eval(this.responseText));
-                requesting=0;
-                reQueue-=1;				
-             }};                               
-        xhr.send(data);
+function getAPLPhys(){
+	console.log(posQueue.length);
+	return new Promise(function (resolve) {
+		if (reQueue < 5 && posQueue.length < 1000) {
+			requesting=1;
+			reQueue+=1;
+			var data = new FormData();
+			data.append("_callback", "Tick");
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', "/BrazilNut.mipage", true);
+			xhr.setRequestHeader("isAPLJax", true);
+			xhr.onreadystatechange = function() {                                   
+				if (this.readyState == 4 && this.status == 200) {                                       
+					APLJaxReturn(eval(this.responseText));
+					requesting=0;
+					resolve();
+					reQueue-=1;				
+				 }
+			};                               
+			xhr.send(data);
+		}
+	});
+}
 
-    }
-    
+function Rnext() {
+    //RenderCanvas();
+	RenderBabylon();    
+    getAPLPhys();
     Ttimer = setTimeout("Rnext()", 17);
 }
 
-//function createAtom(){
-	//console.log("createAtom");
-	
-	
-    //for (i=0;i<pos[0].length;i++){
-		
-	//}	
-	
-	
-	//for (i=0;i<pos.length;i++){
-//	atoms.push(atom);
-//	scene.graph.push(atom);
-	//}
-	//		pos[i] = pos[i].map((a, j) => a * scale);
-			
-		//	atom.translate(pos[i]);				
-//}
+function stepScene() {
+	pos = posQueue.shift();
+	//console.log(pos);
+	for (i=0;i<pos.length;i++){
+		var atom = scene.getMeshByID("atom"+i);
+		//console.log(atom);
+		atom.position.x = pos[i][0]*scale;
+		atom.position.y = pos[i][1]*scale;
+		atom.position.z = pos[i][2]*scale;
+	}
+}
 
-//var atomstyle = Phoria.Entity.createStyle()
-//style = 
+var createScene = function(engine) {
+	// BJS Scene 
+	//scene.dispose();
+	var scene = new BABYLON.Scene(engine);
+	// Scene optimisations https://doc.babylonjs.com/how_to/optimizing_your_scene
+	scene.autoClear = false; // Color buffer
+	scene.autoClearDepthAndStencil = false; // Depth and stencil, obviously
+	scene.blockMaterialDirtyMechanism = true; 
+	scene.useMaterialMeshMap = true;
+	AM = new BABYLON.ActionManager(scene);
+	//var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+	var camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 10, new BABYLON.Vector3(0,0,0), scene);
+	//camera.attachControl(canvas, true);
+	// target the camera to scene origin
+	camera.setTarget(new BABYLON.Vector3(0,10,10));
+	// attach the camera to the canvas
+	camera.attachControl(canvas, true);
+	// create a basic light, aiming 0,1,0 - meaning, to the sky
+	var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+	// create a built-in "ground" shape;
+	var ground = BABYLON.Mesh.CreateGround('ground1', 10, 10, 10, scene);
+	ground.position = new BABYLON.Vector3(5,0,5);
+	var groundmesh = new BABYLON.StandardMaterial("groundmesh", scene);
+	groundmesh.wireframe = true;
+	ground.material = groundmesh;
+
+	//for (var i=0;i<pos.length;i++){
+	//	var sphere = BABYLON.Mesh.CreateSphere('atom', 2, 0.5, scene);
+	//}
+	pos = posQueue.shift();
+	//var particles = new BABYLON.ParticleSystem("particles", pos.length, scene);
+	// Create atoms in scene
+	var sphere = BABYLON.Mesh.CreateSphere('atom', 1, 0.3, scene);
+	var spheremat = new BABYLON.StandardMaterial("spheremat", scene);
+	//spheremat.wireframe = true;
+	spheremat.alpha = 0.5;
+	sphere.material = spheremat;
+	sphere.doNotSyncBoundingInfo = true;
+	sphere.cullingStrategy = BABYLON.AbstractMesh.CULLINGSTRATEGY_BOUNDINGSPHERE_ONLY;
+	sphere.isVisible = false;
+	for (i=0;i<pos.length;i++){
+		var atom = sphere.createInstance("atom"+i);	
+	}
+	
+	return scene;
+}
+
+async function WakeBaby() {
+	// Dispose of old scene
+	//scene.dispose();
+	// Get first set of frames to render
+	posQueue = [];
+	await getAPLPhys();
+	console.log("Baby awake");	
+	// Create engine and scene, dispose of old ones if applicable
+	try {
+		scene.dispose();
+		engine.dispose();
+	}
+	catch (error) {
+		baby = new BABYLON.Engine(canvas, true);
+		scene = createScene(baby);
+	}
+	//scene = createScene(baby);
+	//scene.clearColor = BABYLON.Color3.Blue();
+	// TODO: What is (,true)?
+	//return baby;
+}
+
+function RenderBabylon() {
+	if (posQueue.length === 0) {return;}
+	//console.log(scene);
+	stepScene();
+	// Create Babylon scene 
+	//var scene = createScene(baby);
+	baby.runRenderLoop(function(){
+		scene.render();
+	});
+}
 
 function RenderCanvas() {
 
@@ -113,28 +187,9 @@ function RenderCanvas() {
     }));
     scene.graph.push(new Phoria.DistantLight());
 
-    
-	//pos.forEach(pushAtom(pos, b, scene));
-	// Loop over particles
-	//console.log("Render");
-	//var s = Phoria.Util.generateSphere(0.1,5,5);
-	// for (atom of atoms){
-		// console.log(atom);
-		// atom.translate()
-	// }
 	var s = Phoria.Util.generateSphere(0.1,5,5);
-	// var atomstyle = Phoria.Entity.createStyle({
-		// points: s.points,
-		// edges: s.edges,
-		// polygons: s.polygons,
-		// style:{
-			// drawmode: "solid",
-			// diffuse: 1,
-			// specular: 16,
-			// shademode: "lightsource"
-		// }
-	// });
-	for (i=0;i<pos.length;i++){
+
+	for (var i=0;i<pos.length;i++){
 		var atom = Phoria.Entity.create({
 			points: s.points,
 			edges: s.edges,
@@ -148,29 +203,7 @@ function RenderCanvas() {
 		//atom.style = atomstyle;
 		scene.graph.push(atom);
 		pos[i] = pos[i].map((a, j) => a * scale);
-		// console.log(atoms[i]);
-		//console.log(atoms.length);
-		// atoms[i].translate(pos[i]);
-		//
-		//atom.translate(pos[i]);
-		// 3D Spheres for pretty particles
-		
-		//var axes = Object.keys(s.points[0]);
-		// Loop over sphere defining points
-		//for (j=0;j<s.points.length;j++) {
-		// var atom = Phoria.Entity.create({
-			// points: s.points,
-			// edges: s.edges,
-			// polygons: s.polygons,
-			// style:{
-				// diffuse: 1,
-				// specular: 16,
-				// shademode: "lightsource"
-				// }
-		// });
-		//console.log(scene.graph[i]);
-		
-		
+
 		atom.translate(pos[i]);				
 		//}
 	}
